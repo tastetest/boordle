77c2f94499bf5d7513ac997b225316a2a529d47d
tastetest <tastytothesupreme@gmail.com>
2023-03-12 19:22:03 -0600

    split characters into array elements

--- src/components/RandomWordGen.tsx
+++ src/components/RandomWordGen.tsx
@@ -5,9 +5,10 @@ import { useEffect, useState } from 'react';
 const RandomWordGen = () => {
 
 	let item = words[Math.floor(Math.random() * words.length)];
-	// then split specified item into characters
-	// item.splice('0, 4') -- we want to make sure the word is only a 5 letter word
-	console.log(item);
+	// then put item into an array && split specified item into characters
+	let array = Array.from(item);
+
+	console.log(array);
 
 	return (
 		<>
