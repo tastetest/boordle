f8c963e46cdc47b6a9470fb7239378277cc1c0fc
tastetest <tastytothesupreme@gmail.com>
2023-03-13 14:36:15 -0600

    added gamelogic

---
+++ gameLogic.ts
@@ -0,0 +1,50 @@
+import words from './src/words';
+
+interface GameState {
+	word: string,
+	guesses: string[];
+	attemptsLeft: any;
+}
+
+function generateRandomWord(): string {
+	const index = Math.floor(Math.random() * words.length);
+	return words[index];
+}
+
+function checkGuess(word: string, guesses: string[]): boolean[] {
+	const result: boolean[] = [];
+
+	for (let i = 0; i < word.length; i++) {
+		result.push(guesses.includes(word[i]));
+	}
+	return result;
+}
+
+function isGamewon(word: string, guesses: string[]): boolean {
+	return checkGuess(word, guesses).every((isCorrect) => isCorrect);
+}
+
+function isGameOver(attemptsLeft: number): boolean {
+	return attemptsLeft === 0;
+}
+
+function makeGuess(guess: string, gameState: GameState): GameState {
+	const guesses = [...gameState.guesses, guess];
+	const attemptsLeft = gameState.attemptsLeft - 1;
+	return {
+		...gameState,
+		guesses,
+		attemptsLeft,
+	};
+}
+
+function initializeGameState(): GameState {
+	const word = generateRandomWord();
+	const guesses = [];
+	const attemptsLeft = 6;
+	return {
+		word,
+		guesses,
+		attemptsLeft,
+	};
+}
--- src/components/RandomWordGen.tsx
+++ src/components/RandomWordGen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import words from '../words'
 import { useEffect, useState } from 'react';
+import words from '../words';
+import { Input, Space } from 'antd';
 
 const RandomWordGen = () => {
 
+	const { Search } = Input;
+	let mod;
+	const onSearch = (value: string) => console.log(value);
 	let item = words[Math.floor(Math.random() * words.length)];
+
 	// then put item into an array && split specified item into characters
 	let array = Array.from(item);
 
@@ -20,6 +25,14 @@ const RandomWordGen = () => {
 				<li>
 				</li>
 			</ul>
+			<Search 
+				placeholder="hi"
+				allowClear
+				enterButton="Search"
+				size="large"
+				onSearch={onSearch}
+			/>
+
 		</>
 
 	)
